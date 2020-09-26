3e3c23dade2561c7e8589830f2c89fab48b80d2e
Trevor Otterson <totterson97@gmail.com>
2020-09-26 09:16:40 -0600

    deploying

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
 
 const PORT = process.env.PORT || 8080
 
 const app = express();
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
